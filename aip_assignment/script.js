4d5cbd9d960c596ec325be82f75f84050385ae3f
canonallan <133584930+canonallan@users.noreply.github.com>
2023-12-22 08:41:37 +0300

    Update script.js

--- aip_assignment/script.js
+++ aip_assignment/script.js
@@ -1,3 +1,6 @@
+// SCT211-0019/2020 -->
+//CHIKOMBE CANON ALLAN -->
+
 function renderInformation() {
     const additionalInfo = document.getElementById('additionalInfo').value;
     alert(`Additional Information: ${additionalInfo}`);
